56db86a379675078d446fb4f862b7eecbf54cd27
carinamach <carina.mach1999@gmail.com>
2025-03-18 09:18:43 +0100

    s

--- ecom1/vite.config.js
+++ ecom1/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://carinamach.github.io/examensarbete/examensarbete/ecom1'
+  base: 'examensarbete/examensarbete/ecom1'
 })
