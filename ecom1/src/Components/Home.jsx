da8db054ffff1965d7536f89f3da97171cafb8bc
carinamach <carina.mach1999@gmail.com>
2025-03-18 14:23:20 +0100

    `Added link to view more products on the Home page`

--- ecom1/src/Components/Home.jsx
+++ ecom1/src/Components/Home.jsx
@@ -21,6 +21,13 @@ const Home = () => {
                         <div className="col-lg-6 mx-auto">
                             <p className="lead mb-4">Upptäck charmiga och personliga illustrationer som sprider glädje.</p>
                             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+                            <Link 
+              to="/products" 
+              className="btn btn primary"
+              aria-label="Visa alla produkter"
+            >
+              Fler Produkter
+            </Link>
                             <Link to="/" className="navbar-brand fw-bold">Happy Scribbles</Link>
 
                                 <Link href="/products" className="btn primary btn-lg px-4 me-sm-3">Utforska Butiken</Link>
